16a16e425032fce4356952133aae63603238e319
CircuitCoder <circuitcoder0@gmail.com>
2015-05-01 20:46:43 +0800

    Initialization & Basic skeleton

---
+++ public/javascripts/action.js
@@ -0,0 +1,32 @@
+var app = angular.module("THSITLab", [
+    'ngMaterial',
+    'eee-c.angularBindPolymer'
+]).config(function($mdThemingProvider) {
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('blue', {
+      'default': '500',
+      'hue-1': '300',
+      'hue-2': '900',
+      'hue-3': '100'
+    });
+});
+
+app.controller('AppCtrl', function($scope,$timeout,$mdSidenav) {
+  $scope.pageTitle = "主页";
+  $scope.openSidenav = function(menuId) {
+    $mdSidenav('main')
+      .toggle()
+      .then(function() {
+        console.log('Main sidebar toggled');
+      });
+  };
+}).controller("NavMain", function($scope,$timeout,$mdSidenav) {
+  $scope.close = function() {
+    $mdSidenav('main')
+      .close()
+      .then(function() {
+        console.log('Main sidebar closed');
+      });
+  }
+});
