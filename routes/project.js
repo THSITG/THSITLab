69fc601d9d4fb7b710eaf40ddaa6ffc5fd97a749
Liu Xiaoyi <circuitcoder0@gmail.com>
2015-05-10 17:47:13 +0800

    Fixed syntax error

--- routes/project.js
+++ routes/project.js
@@ -2,39 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* Request to create a new project */
-route.post('/new', function(req, res, next) {
+router.post('/new', function(req, res, next) {
 });
 
 /* Request to delete a new project */
-route.delete('/:id', function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
 });
 
 /* Get the status of a specified project */
-route.get('/:id/status', function(req, res, next) {
+router.get('/:id/status', function(req, res, next) {
 });
 
 /* Activate of deactivate a specified project */
-route.post('/:id/status', function(req, res, next) {
+router.post('/:id/status', function(req, res, next) {
 });
 
 /* Get all cards of a specified project */
-route.get('/:id/cards', function(req, res, next) {
+router.get('/:id/cards', function(req, res, next) {
 });
 
 /* Create a new card */
-route.post('/:id/cards/new', function(req, res, next) {
+router.post('/:id/cards/new', function(req, res, next) {
 });
 
 /* Get a card's content */
-route.get('/:id/cards/:cid', function(req, res, next) {
+router.get('/:id/cards/:cid', function(req, res, next) {
 });
 
 /* Update a specified card */
-route.post('/:id/cards/:id', function(req, res, next) {
+router.post('/:id/cards/:id', function(req, res, next) {
 });
 
 /* Delete a card */
-route.delete('/:id/cards/:cid', function(req, res, next) {
+router.delete('/:id/cards/:cid', function(req, res, next) {
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,19 @@ router.post('/login', function(req, res, next) {
 });
 
 /* logout API */
-route.post('/logout', function(req, res, next) {
+router.post('/logout', function(req, res, next) {
 });
 
 /* Get the profile of a specified user */
-route.get('/:id/profile', function(req, res, next) {
+router.get('/:id/profile', function(req, res, next) {
 });
 
 /* Update a user's profile */
-route.post('/:id/profile', function(req, res, next) {
+router.post('/:id/profile', function(req, res, next) {
 });
 
 /* Get the projects of a specified user */
-route.get('/:id/projects', function(req, res, next) {
-}
+router.get('/:id/projects', function(req, res, next) {
+});
 
 module.exports = router;
